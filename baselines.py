34ab63270f965bc3a2bf5c9c27a94c5907e9efff
Alexandru Coca <alexcoca23@yahoo.co.uk>
2018-07-19 18:39:01 +0100

    Fixed bugs in the DPRegression() class

--- baselines.py
+++ baselines.py
@@ -8,7 +8,7 @@ Created on Thu May 31 14:31:11 2018
 import numpy as np
 import mlutilities as utils
 
-class Adassp():
+class DPRegression():
     def __init__(self):
         '''Params 
         @epsilon: privacy budget
@@ -23,9 +23,13 @@ class Adassp():
         self.X = []
         self.y = []
         self.reg_parameters = []
-        np.random.seed(seed=5)
+        self.rho = 0.01
+        self.epsilon = 0.1
+        self.X_bound = 1
+        self.y_bound = 1
         
-    def set_privacy_parameters(self,epsilon,n,X_bound=-1,y_bound=-1):
+        
+    def set_privacy_parameters(self, epsilon, num_records, X_bound = 1, y_bound = 1):
         ''' This method can be used to set privacy parameter epsilon. Delta is automatically set.
         @ Parameters:
             epsilon: privacy budget
@@ -33,22 +37,15 @@ class Adassp():
             X_bound: feature space bound (sup ||x||_2 over the data universe X)
             y_bound: target space bound (abs(y) if y is scalar, otherwise as per X_bound ''' 
         self.epsilon = epsilon
-        self.delta = np.min(1e-6,1/n[0])
+        self.delta = np.minimum(1e-6, 1/num_records)
         self.rho = 0.01 # Bound on the error for ADASSP hold with prob 1 - rho
         self.X_bound = X_bound
+        self.y_bound = y_bound
         
-    def release_regression_coef(self,X,y):
+    def release_regression_coef(self, X, y):
         ''' This method releases regression coeffients with differential privacy.
         The method used is sufficient statistics pertrurbation with adaptive damping (#TODO: add ref)
         '''    
-
-        # Compute feature space bound 
-        if self.X_bound == -1:
-            self.X_bound = utils.compute_bound(X)
-        
-        # Compute target space bound
-        if self.y_bound == -1:
-            self.y_bound = utils.compute_bound(y)
             
         # Helper constants
         priv_const = (np.sqrt(np.log(6/self.delta))/(self.epsilon/3))*(self.X_bound**2)
@@ -61,23 +58,37 @@ class Adassp():
         lambda_min = np.min(np.linalg.eig(Sigma)[0]) 
         
         # Privately release smallest eigenvalue
-        lambda_private = np.max([lambda_min + priv_const*np.random.normal()  - priv_const*np.sqrt(6/self.delta),0])
+        lambda_private = np.max([lambda_min + priv_const*np.random.normal() - priv_const*np.sqrt(6/self.delta),0])
         
         # Calculate regularisation parameter
-        reg_param = np.max([priv_const*np.sqrt(d*np.log(2*d**2/self.rho))- lambda_private,0])
+        reg_param = np.max([priv_const*np.sqrt(d*np.log(2*d**2/self.rho)) - lambda_private,0])
         
         # Privately release covariance matrix 
         Z = utils.generate_sym_rand_matrix(d)
         Sigma_private = Sigma + priv_const*Z
         
         # Privately release X^Ty 
-        Z = np.random.normal(size=(d,))
-        vec_priv = np.traspose(X)@y + (priv_const/self.X_bound)*self.y_bound*Z
+        Z = np.random.normal(size=(d, 1))
+        vec_priv = np.transpose(X)@y + (priv_const/self.X_bound)*self.y_bound*Z
         
         # Output regression parameters
         reg_parameters = np.linalg.solve(Sigma_private + reg_param*np.eye(d), vec_priv)
         
         return reg_parameters
+    
+    def get_parameters(self, X, y, num_samples, epsilon, seed = 23):
+        
+        np.random.seed(seed)
+        
+        parameters = []
+        # Set privacy paramters 
+        self.set_privacy_parameters(epsilon, X.shape[0])
+        
+        # Release differentially private regression coefficients 
+        for _ in range(num_samples):
+            parameters.append(self.release_regression_coef(X, y))
+        
+        return np.array(parameters)
     
 class Regression():
     
@@ -123,7 +134,6 @@ class Regression():
             tmp = np.tile(test_data[:,:-1],[model_parameters.shape[0], 1, 1])*\
                                 np.transpose(model_parameters, axes = (0, 2, 1))
             predicted_targets = np.sum(tmp, axis = 2, keepdims = True)
-            
             rms_errors = np.sqrt((1/test_data.shape[0])*np.sum((test_data[:,-1:] - predicted_targets)**2, axis = 1))
             
         else:
