6ee46ed787c2f1044f2426a3f02328687e26708c
Alexandru Coca <alexcoca23@yahoo.co.uk>
2018-07-24 17:02:12 +0100

    Added utility vs rmse plots

--- exputils.py
+++ exputils.py
@@ -6,6 +6,8 @@ Created on Fri Jul 20 11:48:59 2018
 """
 
 def extract_data(results):
+    delta_cov_norms_f = []
+    delta_corr_norms_2 = []
     avg_2_norms = []
     double_std_2_norms = []
     avg_f_norms = []
@@ -26,6 +28,8 @@ def extract_data(results):
     # test_set = []
     # private_data = []
     for result in results:
+        delta_cov_norms_f.append(result['delta_cov_norms_f'])
+        delta_corr_norms_2.append(result['delta_corr_norms_2'])
         avg_2_norms.append(result['avg_2_norm_corr'])
         double_std_2_norms.append(2*result['std_2_norm_corr'])
         avg_f_norms.append(result['avg_f_norm_cov'])
@@ -46,11 +50,13 @@ def extract_data(results):
        # private_data.append(result['private_data'])
     test_set = results[0]['test_set']
     private_data = results[0]['private_data']    
-    return avg_2_norms, double_std_2_norms, avg_f_norms, double_std_f_norms, max_utilities, sample_utilities, max_sampled_utilities, min_sampled_utilities, \
+    return delta_cov_norms_f, delta_corr_norms_2, avg_2_norms, double_std_2_norms, avg_f_norms, double_std_f_norms, max_utilities, sample_utilities, max_sampled_utilities, min_sampled_utilities, \
             avg_samples_utility, double_std_utility, avg_samples_score, double_std_score, synthetic_datasets, delta_opt_avg,\
             delta_opt_best, delta_opt_worst, test_set, private_data
 
 def initialise_netmech_containers(epsilon_vec):
+    delta_cov_norms_f = {key: [] for key in epsilon_vec}
+    delta_corr_norms_2 = {key: [] for key in epsilon_vec}
     avg_2_norms = {key: [] for key in epsilon_vec}
     double_std_2_norms = {key: [] for key in epsilon_vec}
     avg_f_norms = {key: [] for key in epsilon_vec}
@@ -69,7 +75,7 @@ def initialise_netmech_containers(epsilon_vec):
     delta_opt_worst = {key: [] for key in epsilon_vec}
     test_set = {key: [] for key in epsilon_vec}
     private_data = {key: [] for key in epsilon_vec}
-    return avg_2_norms, double_std_2_norms, avg_f_norms, double_std_f_norms, max_utilities, sample_utilities, max_sampled_utilities, min_sampled_utilities, \
+    return delta_cov_norms_f, delta_corr_norms_2, avg_2_norms, double_std_2_norms, avg_f_norms, double_std_f_norms, max_utilities, sample_utilities, max_sampled_utilities, min_sampled_utilities, \
             avg_samples_utility, double_std_utility, avg_samples_score, double_std_score, synthetic_datasets_vec, delta_opt_avg,\
             delta_opt_best, delta_opt_worst, test_set, private_data
 
